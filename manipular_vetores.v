3728b818d5fcb2844613ff9b6cf4c87f64bb2fe1
KaioMenezesB <kaio_borduque@hotmail.com>
2025-04-15 07:55:16 -0300

    Atividade manipular vetores finalizada

--- manipular_vetores.v
+++ manipular_vetores.v
@@ -2,6 +2,11 @@ module manipular_vetores(
     input [31:0] entrada,
     output [31:0] saida );
 
-    // insira seu código aqui
+    assign saida = { 
+        entrada[ 7: 0],  // byte menos significativo vira o mais significativo
+        entrada[15: 8],
+        entrada[23:16],
+        entrada[31:24]   // byte mais significativo vira o menos significativo
+    };
 
 endmodule
